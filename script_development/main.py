9db86e0d6b50cc7240df393eb2d2a4372c5cf90e
Germonda <germondareijnen@gmail.com>
2017-05-05 17:13:16 +0200

    main.py with some 3D volume loading and visualization added

---
+++ script_development/main.py
@@ -0,0 +1,409 @@
+
+import os
+import numpy as np
+np.set_printoptions(precision=2, suppress=True)
+import matplotlib.pyplot as plt
+import nibabel as nib
+from nibabel.testing import data_path
+
+#import nilearn
+#from nilearn import plotting
+
+import os
+import numpy as np
+from PIL import Image
+from math import cos, sin, radians, floor, ceil
+
+import theano.tensor as T
+import lasagne
+import lasagne.layers as L
+
+import random
+
+import matplotlib
+import matplotlib.pyplot as plt
+matplotlib.rcParams['figure.figsize'] = (40, 24)
+matplotlib.rcParams['xtick.labelsize'] = 30
+from IPython import display
+import time
+#from tqdm import tnrange, tqdm_notebook
+
+#from SampleExtractor import visualize_samples
+from SampleExtractor import SampleExtractor
+from BatchExtractor import BatchExtractor
+from fcn import build_network
+from fcn import softmax
+from fcn import softmax_deterministic
+from fcn import training_function
+from fcn import validate_function
+from fcn import evaluate_function
+from tools import get_file_list
+from tools import show_image
+
+#plt.isinteractive()
+
+'''
+Data loading and visualization
+'''
+def show_slices(vol_slices, seg_slices):
+    """ Function to display row of image slices """
+    nslice = len(vol_slices)
+    fig, axes = plt.subplots(2, nslice)
+    for i, slice in enumerate(vol_slices):
+        axes[0, i].imshow(slice.T, cmap="gray", origin="lower")
+    for i, slice in enumerate(seg_slices):
+        axes[1, i].imshow(slice.T, cmap="gray", origin="lower", clim=(0, 2))
+
+train_batch_1_dir='./Training_Batch_1'
+
+vol_batch_1 = sorted(get_file_list(train_batch_1_dir, 'volume')[0])
+seg_batch_1 = sorted(get_file_list(train_batch_1_dir, 'segmentation')[0])
+
+#for i, (vol, seg) in enumerate(zip(vol_batch_1, seg_batch_1)):
+
+i = 0
+vol = vol_batch_1[i]
+seg = seg_batch_1[i]
+
+#vol_filename = vol_batch_1[i]
+vol_proxy = nib.load(vol)
+vol_array = vol_proxy.get_data()
+#seg_filename = seg_batch_1[9]
+seg_proxy = nib.load(seg)
+seg_array = seg_proxy.get_data()
+mid_0 = int(vol_array.shape[0]/2)
+mid_1 = int(vol_array.shape[1]/2)
+mid_2 = int(vol_array.shape[2]/2)
+vol_slice_0 = vol_array[mid_0, :, :]
+vol_slice_1 = vol_array[:, mid_1, :]
+vol_slice_2 = vol_array[:, :, mid_2]
+seg_slice_0 = seg_array[mid_0, :, :]
+seg_slice_1 = seg_array[:, mid_1, :]
+seg_slice_2 = seg_array[:, :, mid_2]
+show_slices([vol_slice_0, vol_slice_1, vol_slice_2], [seg_slice_0, seg_slice_1, seg_slice_2])
+plt.suptitle("Center slices for 3D volume "+vol, size=40)
+plt.show(block=False)
+plt.pause(0.5)
+
+
+'''
+Split the data in a training and validation set.
+'''
+
+validation_percentage = 0.3
+n_validation_images = int(ceil(len(vol_batch_1)*validation_percentage))
+
+# use the first images as validation
+val_vol_batch_1 = vol_batch_1[0:n_validation_images]
+val_seg_batch_1 = seg_batch_1[0:n_validation_images]
+
+# the rest as training
+tra_vol_batch_1 = vol_batch_1[n_validation_images:]
+tra_seg_batch_1 = seg_batch_1[n_validation_images:]
+
+print("nr of training images: " + str(len(tra_vol_batch_1)) + "\nnr of validation images: " + str(len(val_vol_batch_1)))
+
+# Show the training set using the function defined
+#for i in range(len(tra_imgs)):
+#    show_image(i, tra_imgs, tra_msks, tra_lbls)
+
+'''
+Creating and testing the basic random sample extractor
+'''
+
+# Implement the get_random_sample_from_class() function in the class above. You can use the functions below to test if the output makes sense.
+
+# Here we test the patch extractor without random rotation and without flipping.
+
+# define a random sample extractor object for the first 3D volume
+random_sample_extractor = SampleExtractor((512,512), tra_vol_batch_1[0], tra_seg_batch_1[0], max_rotation=0, rnd_flipping=False)
+
+# Execute the following cells several times to see if the results you get make sense.
+
+X_0 = []
+Y_0 = []
+for i in range(5):
+    X, Y = random_sample_extractor.get_random_sample_from_class(0)
+    X_0.append(X)
+    Y_0.append(Y)
+
+show_slices(X_0, Y_0)
+plt.suptitle("Label=0 slices: no liver", size=40)
+
+plt.show(block=False)
+
+X_1 = []
+Y_1 = []
+for i in range(5):
+    X, Y = random_sample_extractor.get_random_sample_from_class(1)
+    X_1.append(X)
+    Y_1.append(Y)
+
+show_slices(X_1, Y_1)
+plt.suptitle("Label=1 slices: liver without lesions", size=40)
+
+plt.show(block=False)
+
+X_2 = []
+Y_2 = []
+for i in range(5):
+    X, Y = random_sample_extractor.get_random_sample_from_class(2)
+    X_2.append(X)
+    Y_2.append(Y)
+
+show_slices(X_2, Y_2)
+plt.suptitle("Label=2 slices: liver lesions", size=40)
+
+plt.show(block=False)
+
+# ## Implement rotation augmentation
+#
+# Implement the ```get_rnd_rotation()``` function in the class above.
+# You can use the functions below to test if the output makes sense.
+
+
+#Test if we can generate instances of the sample extractor class for all 3D volumes
+sample_extractors=[]
+for i, (vol, seg) in enumerate(zip(tra_vol_batch_1, tra_seg_batch_1)):
+    sample_extractor = SampleExtractor((512, 512),vol, seg, max_rotation=0, rnd_flipping=False)
+#    exec "sample_extractor_%s=sample_extractor" % (i)
+#    sample_extractors.append(sample_extractor)
+
+
+#Now we enable rotation, and test it again.
+random_sample_extractor = SampleExtractor((31,31),tra_imgs, tra_msks, tra_lbls, max_rotation=15, rnd_flipping=False)
+
+# visualize example of vessels (label = 1)
+for i in range(5):
+    X, Y = random_sample_extractor.get_random_sample_from_class(1)
+    plt.subplot(1, 5, i + 1)
+    plt.imshow(X, cmap='bone', vmin=0, vmax=1)
+    plt.title('patch: ' + str(i) + " , class: " + str(Y))
+
+# visualize example of background (label = 0)
+for i in range(5):
+    X, Y = random_sample_extractor.get_random_sample_from_class(0)
+    plt.subplot(1, 5, i + 1)
+    plt.imshow(X, cmap='bone', vmin=0, vmax=1)
+    plt.title('patch: ' + str(i) + " , class: " + str(Y))
+
+# ## Implement mirroring augmentation
+# Implement the ```rnd_flip()`` function in the class above.
+# You can use the functions below to test if the output makes sense.
+
+random_sample_extractor = SampleExtractor((31,31),tra_imgs, tra_msks, tra_lbls, max_rotation=0, rnd_flipping=True)
+
+# visualize example of vessels (label = 1)
+for i in range(5):
+    X, Y = random_sample_extractor.get_random_sample_from_class(1)
+    plt.subplot(1, 5, i + 1)
+    plt.imshow(X, cmap='bone', vmin=0, vmax=1)
+    plt.title('patch: ' + str(i) + " , class: " + str(Y))
+
+# visualize example of background (label = 0)
+for i in range(5):
+    X, Y = random_sample_extractor.get_random_sample_from_class(0)
+    plt.subplot(1, 5, i + 1)
+    plt.imshow(X, cmap='bone', vmin=0, vmax=1)
+    plt.title('patch: ' + str(i) + " , class: " + str(Y))
+
+'''
+Set parameters
+Set the parameters to suitable values.
+Certain paramters have a large influence on the performance of your network, try to find the optimal parameters for
+this problem!
+'''
+
+learning_rate = 0.01
+nr_epochs = 10
+nr_iterations_per_epoch = 1000
+patch_size = (31,31)
+batch_size = 100
+max_rotation = 15
+flipping = True
+class_balancing = True
+nr_validation_samples = 1000
+validation_batch_size = 100
+network_name = 'network_ass7'
+
+
+# ## Compile the theano functions
+inputs = T.ftensor4('X')
+targets = T.ftensor4('Y')
+
+network = build_network(inputs)
+
+train_fn = training_function(network=network, input_tensor=inputs, target_tensor=targets, learning_rate=learning_rate,
+                             use_l2_regularization=False, l2_lambda=0.000001)
+validation_fn = validate_function(network=network, input_tensor=inputs, target_tensor=targets)
+evaluation_fn = evaluate_function(network=network, input_tensor=inputs)
+
+'''
+Create the
+ - sample extractors
+ - batch extractors
+ - constant validation set
+ - Initialize all monitoring statistics
+'''
+
+random.seed(0)
+
+
+sample_ex_train = SampleExtractor(patch_size, tra_vol_batch_1[0], tra_seg_batch_1[0], max_rotation=max_rotation, rnd_flipping=flipping)
+batch_ex_train = BatchExtractor(sample_ex_train, batch_size, class_balancing=class_balancing)
+
+sample_ex_val = SampleExtractor(patch_size, val_imgs, val_msks, val_lbls, max_rotation=max_rotation, rnd_flipping=flipping)
+batch_ex_val = BatchExtractor(sample_ex_val, nr_validation_samples, class_balancing=class_balancing)
+
+validation_X, validation_Y = batch_ex_val.get_random_batch_balanced()
+
+tra_loss_lst = []
+val_loss_lst = []
+tra_acc_lst = []
+val_acc_lst = []
+best_val_acc = 0
+
+'''
+Main training loop
+
+Now that we have a pipeline for creating batches on the fly, we can now train our fully convolutional network!
+The following code trains for a certain amount of time, defined in the parameters section above, and will then evaluate
+the performance on our validation set. After plotting the results, the next epoch will start.
+Try to optimize the parameters to get the best performance possible! (>90% accuracy should be possible!)
+'''
+
+# plot learning curves
+fig = plt.figure(figsize=(30, 15))
+plt.xlabel('epoch', size=40)
+plt.ylabel('loss', size=40)
+fig.labelsize=40
+# Main training loop
+for epoch in range(nr_epochs):
+    print('Epoch {}'.format(epoch + 1))
+    # training
+    tra_losses = []
+    tra_accs = []
+    print('training...')
+    for b in range(0, nr_iterations_per_epoch),:
+        X, Y = batch_ex_train.get_random_batch_balanced()
+        # print 'train X', X.shape, X.dtype, X.min(), X.max(), np.any(np.isnan(X))
+        # print 'train Y', Y.shape, Y.dtype, Y.min(), Y.max(), np.any(np.isnan(Y))
+        loss, accuracy = train_fn(X.astype(np.float32), Y.astype(np.float32))
+        # print 'train loss, accuracy', loss, accuracy
+        tra_losses.append(loss)
+        tra_accs.append(accuracy)
+    tra_loss_lst.append(np.mean(tra_losses))
+    tra_acc_lst.append(np.mean(tra_accs))
+
+    # validation
+    val_losses = []
+    val_accs = []
+    print('validation...')
+    # print validation_batch_size, nr_validation_samples
+    # print validation_X.shape, validation_Y.shape
+
+    for b in range(0, nr_validation_samples, validation_batch_size):
+        X = validation_X[b:min(b + validation_batch_size, nr_validation_samples)]
+        Y = validation_Y[b:min(b + validation_batch_size, nr_validation_samples)]
+        # print 'test X', X.shape, X.dtype#, X.min(), X.max(), np.any(np.isnan(X))
+        # print 'test Y', Y.shape, Y.dtype, Y.min(), Y.max(), np.any(np.isnan(Y))
+        loss, accuracy = validation_fn(X.astype(np.float32), Y.astype(np.float32))
+        # print 'test loss, accuracy', loss, accuracy
+        val_losses.append(loss)
+        val_accs.append(accuracy)
+    val_loss_lst.append(np.mean(val_losses))
+    val_acc_lst.append(np.mean(val_accs))
+    # print val_accs
+    # continue
+    if np.mean(val_accs) > best_val_acc:
+        best_val_acc = np.mean(val_accs)
+        # save network
+        params = L.get_all_param_values(network)
+        np.savez(os.path.join('./', network_name + '.npz'), params=params)
+
+    # plot learning curves
+    #fig = plt.figure(figsize=(10, 5))
+    tra_loss_plt, = plt.plot(range(len(tra_loss_lst)), tra_loss_lst, 'b')
+    val_loss_plt, = plt.plot(range(len(val_loss_lst)), val_loss_lst, 'g')
+    tra_acc_plt, = plt.plot(range(len(tra_acc_lst)), tra_acc_lst, 'm')
+    val_acc_plt, = plt.plot(range(len(val_acc_lst)), val_acc_lst, 'r')
+    #plt.xlabel('epoch')
+    #plt.ylabel('loss')
+    #plt.legend([tra_loss_plt, val_loss_plt, tra_acc_plt, val_acc_plt],
+    #           ['training loss', 'validation loss', 'training accuracy', 'validation accuracy'],
+    #           loc='center left', bbox_to_anchor=(1, 0.5))
+    plt.title('Best validation accuracy = {:.2f}%'.format(100. * best_val_acc), size=40)
+    plt.show(block=False)
+    plt.pause(.5)
+
+plt.legend([tra_loss_plt, val_loss_plt, tra_acc_plt, val_acc_plt],
+           ['training loss', 'validation loss', 'training accuracy', 'validation accuracy'],
+           loc='center left', bbox_to_anchor=(1, 0.5))
+
+'''
+Evaluate the performance on the test set and submit to Challenger
+
+We can now apply the trained fully convolutional network on our independent test set.
+'''
+
+# Load the images:
+
+# test images
+tes_img_dir = os.path.join(data_dir, 'test', 'images')
+tes_msk_dir = os.path.join(data_dir, 'test', 'mask')
+
+tes_imgs = sorted(get_file_list(tes_img_dir, 'tif')[0])
+tes_msks = sorted(get_file_list(tes_msk_dir, 'gif')[0])
+
+result_output_folder = os.path.join(data_dir, 'test', 'results')
+if not (os.path.exists(result_output_folder)):
+    os.mkdir(result_output_folder)
+
+# ## Apply the trained network to the images
+
+# indicate the name of the network for this test
+network_name = 'network_ass7'
+
+# initialize the network used in this experiment (this may change)
+network = build_network(inputs)
+npz = np.load('./' + network_name + '.npz')  # load stored parameters
+lasagne.layers.set_all_param_values(network, npz['params'])  # set parameters
+
+# one forward pass on the test set
+fig, axes = plt.subplots(nrows=len(tes_imgs), ncols=3, figsize=(15, len(tes_imgs)*5))
+threshold = 0.5
+for f in range(len(tes_imgs)):
+    # open image
+    img = np.asarray(Image.open(tes_imgs[f]))
+
+    # extract green channel and normalize
+    img_g = img[:, :, 1].squeeze().astype(float) / 255.0
+
+    # zero padding to apply fully-convolutional network
+    img_g_padded = np.pad(img_g, ((patch_size[0] // 2, patch_size[0] // 2),
+                                  (patch_size[1] // 2, patch_size[1] // 2)), 'constant', constant_values=[0, 0])
+
+    # forward pass
+    probability = evaluation_fn(np.expand_dims(np.expand_dims(img_g_padded.astype(np.float32), axis=0), axis=0))
+    probability = probability[0, 1, :, :]
+
+    # binary version by thresholding
+    thresholded_image = probability > threshold
+
+    # show results
+    ax0 = plt.subplot2grid((len(tes_imgs), 3), (f, 0))
+    ax1 = plt.subplot2grid((len(tes_imgs), 3), (f, 1))
+    ax2 = plt.subplot2grid((len(tes_imgs), 3), (f, 2))
+    #plt.subplot(1, 3, 1)
+    ax0.imshow(img_g_padded, cmap='gray')
+    #plt.subplot(1, 3, 2)
+    ax1.imshow(probability, cmap='jet')
+    #plt.subplot(1, 3, 3)
+    ax2.imshow(thresholded_image, cmap='gray')
+
+plt.show()
+
+
+
+
+
