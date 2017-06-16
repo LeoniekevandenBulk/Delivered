fixed the variance of the gaussian noise, which was set at an integer which was too high, and now depends on the range of the normalized images.

main/Trainer.py
@@ -208,7 +208,8 @@ class Trainer:
                 X_tra, Y_tra, M_tra = batch
 
                 # Augment data batch
-                X_tra, Y_tra, M_tra = augmenter.getAugmentation(X_tra, Y_tra, M_tra, aug_params)
+		gauss_std=(0.05*(np.max(X_tra)-np.min(X_tra)))
+                X_tra, Y_tra, M_tra = augmenter.getAugmentation(X_tra, Y_tra, M_tra, aug_param, gauss_std=gauss_std)
 
                 # ROI METHOD WERE WE PUT EVERY PIXEL OUTSIDE OF LIVER TO ZERO
                 if(target_class == 'lesion' and not(mask_name == None)):